47d1004bbc51341f3c0dfae4a6ac562c63efc1e7
shadman76r <shadmansakibalam@gmail.com>
2024-10-28 21:03:52 +0600

    validation

---
+++ Lab-1(Validation).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    string input; /// To take the string
+    bool isvalid=true; /// For validation
+    cout<<"Please enter a string to check the validation:"<<endl; /// cout news for validation
+    getline(cin,input); /// To Check all the lines with all of special one words like -_*&^%$#@!
+    if(!((input[0]>='A'&&input[0]<='Z')||(input[0]>='a'&&input[0] <='z')||input[0]=='_')){
+        isvalid = false; /// this one is main
+    }
+    for(int i=1;i<input.size();++i){
+        if(!((input[i]>='A'&&input[i]<='Z')||(input[i]>='a'&&input[i] <='z')||(input[i]>='0'&&input[i]<='9')||input[i]=='_'))
+        {
+            isvalid=false; /// this one is for loop
+            break;
+        }
+    }
+
+    ///to print the full function
+    if(isvalid){
+        cout<<input <<" is a valid string "<<endl; /// to print the strings
+    }
+    else{
+        cout<< input <<" is not a valid string "<<endl;
+    }
+    return 0;
+}
