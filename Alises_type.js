ddb4af3a6827b024e621830043b40069c2b64119
shadman76r <shadmansakibalam@gmail.com>
2024-11-13 23:44:07 +0600

    type-1

---
+++ Alises_type.js
@@ -0,0 +1,2 @@
+var a = { name: 'Shadman', id: 22 - 46262 - 1 };
+console.log(a.name);
---
+++ Alises_type.ts
@@ -0,0 +1,7 @@
+type a=string|number
+type c={
+    name: string
+    id:a
+}
+const a:c={name:'Shadman',id:22-46262-1}
+console.log(a.name);
