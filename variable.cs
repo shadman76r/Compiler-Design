a45dcf22a8a2d0d753857af9f23b6e877aee7fe6
shadman76r <shadmansakibalam@gmail.com>
2024-11-15 01:33:26 +0600

    loop

--- variable.cs
+++ variable.cs
@@ -108,3 +108,23 @@ namespace MyApplication
         }
     }
 }
+//loop
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 10);
+        }
+    }
+}
