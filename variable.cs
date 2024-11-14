1a1a352a61f0d3e1cf49ba93793df3c30220e8ed
shadman76r <shadmansakibalam@gmail.com>
2024-11-15 01:26:13 +0600

    switch

--- variable.cs
+++ variable.cs
@@ -70,3 +70,41 @@ namespace MyApplication
         }
     }
 }*/
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 8;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 8:
+                    Console.WriteLine("monday");
+                    break;            }
+        }
+    }
+}
