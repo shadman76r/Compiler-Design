00607e4d7f9a5c5b585d1fe439b2a9f9bc0b40c8
shadman76r <shadmansakibalam@gmail.com>
2024-11-20 21:19:26 +0600

    current

--- lab_task_compiler_file.cpp
+++ lab_task_compiler_file.cpp
@@ -1,6 +1,8 @@
+///this file id for caheck that the functions are define well or not if not that it can give an answer so this cede is linked with "com_lab_5.cpp"
+
 int main(){
 
 }
 double name(){
-    
+
 }
