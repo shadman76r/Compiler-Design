cf708c072d9cf172f15ad248a35b3107e7f8abbb
shadman76r <shadmansakibalam@gmail.com>
2024-11-21 20:31:48 +0600

    new

--- Graphics/Bresenhams.cpp
+++ Graphics/Bresenhams.cpp
@@ -144,6 +144,75 @@ int main(int argc, char** argv) {
 }
 
 
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <GL/glut.h> // GLUT, include glu.h and gl.h
+using namespace std;
+
+void display() {
+    glClearColor(0.408f, 0.816f, 1.02f, 0.0f); // Background color
+    glClear(GL_COLOR_BUFFER_BIT);             // Clear the buffer
+    gluOrtho2D(-15, 15, -15, 15);             // Set coordinate system
+
+    int x1, y1, x2, y2;
+    cout << "Enter the value of x1: ";
+    cin >> x1;
+    cout << "Enter the value of y1: ";
+    cin >> y1;
+    cout << "Enter the value of x2: ";
+    cin >> x2;
+    cout << "Enter the value of y2: ";
+    cin >> y2;
+
+    glPointSize(4.0f); // Set point size
+    glBegin(GL_POINTS);
+
+    // Step 1: Calculate Δx and Δy
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    // Step 2: Initialize decision parameter
+    int d= 2dy-dx;
+    int dE= 2dy;
+    int dNE= 2*(dy - dx);
+
+    int x = x1, y = y1; // Starting point
+
+    // Step 3: Bresenham's Algorithm
+    while (x <= x2)
+        {
+        glVertex2f(x, y); // Plot the current point
+
+        if (d <= 0)
+        {
+            // Case: Choose E (East)
+            d = d + dE;
+            x = x + 1;
+            y = y ;
+        } else {
+            // Case: Choose NE (North-East)
+            d = d + dNE;
+            x = x + 1; // Update decision parameter
+            y = y + 1;   // Increment y
+        }
+       // Increment x
+    }
+
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(400, 400);        // Set the window's initial width & height
+    glutInitWindowPosition(50, 50);      // Set the window's initial position
+    glutCreateWindow("Bresenham's Line"); // Create a window with the given title
+    glutDisplayFunc(display);            // Register display callback handler
+    glutMainLoop();                      // Enter the event-processing loop
+    return 0;
+}
+
+
 
 
 
