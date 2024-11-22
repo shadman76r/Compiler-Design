493a44ecc621822b61d8d0cc782cdcaa54840685
shadman76r <shadmansakibalam@gmail.com>
2024-11-23 00:57:43 +0600

    c#_lab_task_inhertence

--- Lab_task_c#.cs
+++ Lab_task_c#.cs
@@ -120,3 +120,172 @@ namespace VehicleApp
         }
     }
 }
+
+
+
+//2nd lab task
+
+using System;
+namespace Inheritance
+{
+    public class Person
+    {
+        private string name;
+        private string dob;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Dob
+        {
+            get
+            {
+                return dob;
+            }
+            set
+            {
+                dob = value;
+            }
+        }
+        public Person()
+        {
+            //This is called difalut constractor
+        }
+        public Person(string name, string dob)
+        {
+            this.name = name;
+            this.dob = dob;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setDob(string dob)
+        {
+            this.dob = dob;
+        }
+        public string getDob()
+        {
+            return dob;
+        }
+    }
+    public class Account
+    {
+        private int account_id;
+        private double amount;
+        private string account_type;
+
+        public int Account_id
+        {
+            get
+            {
+                return account_id;
+            }
+            set
+            {
+                this.Account_id = value;
+            }
+        }
+        public double Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                this.Amount = value;
+            }
+        }
+        public string Account_Type
+        {
+            get
+            {
+                return account_type;
+            }
+            set
+            {
+                this.account_type = value;
+            }
+        }
+        public Account(int account_id, double amount, string account_type)
+        {
+            this.account_id = account_id;
+            this.amount = amount;
+            this.account_type = account_type;
+        }
+    }
+    public class Employe : Person
+    {
+        private Account acc;
+        public Employe()
+        {
+            //This is called difalut constractor
+        }
+        public Employe(string name, string dob, int account_id, double amount, string account_type) : base(name, dob)
+        {
+            this.acc = new Account(account_id, amount, account_type);
+        }
+        public void Display()
+        {
+            Console.WriteLine("The Employe name            : " + getName());
+            Console.WriteLine("The Employe dob             : " + getDob());
+            Console.WriteLine("The Employe Account Number  : " + acc.Account_id);
+            Console.WriteLine("The Employe Account Balance : " + acc.Amount);
+            Console.WriteLine("The Employe Account Type    : " + acc.Account_Type);
+        }
+    }
+    public class Customer : Person
+    {
+        private Account acc;
+        public Customer()
+        {
+            //This is called difalut constractor
+        }
+        public Customer(string name, string dob, int account_id, double amount, string account_type) : base(name, dob)
+        {
+            this.acc = new Account(account_id, amount, account_type);
+        }
+        public void Display()
+        {
+            Console.WriteLine("The Customer name            : " + getName());
+            Console.WriteLine("    Customer dob             : " + getDob());
+            Console.WriteLine("    Customer Account Number  : " + acc.Account_id);
+            Console.WriteLine("    Customer Account Balance : " + acc.Amount);
+            Console.WriteLine("    Customer Account Type    : " + acc.Account_Type);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employe obj1 = new Employe("Nitu", "18-02-2004", 16555874, 2000000.00, "Personal");
+            Employe obj2 = new Employe("Mitu", "22-06-2004", 11414168, 100000.00, "Personal");
+        
+
+            Customer obj3 = new Customer("Farzana", "21-08-2004", 13158716, 5000000.00, "Savings");
+            Customer obj4 = new Customer("Akhi", "13-06-1999", 984513202, 5589335.21, "Political");
+
+
+            obj1.Display();
+            Console.WriteLine();
+            obj2.Display();
+            Console.WriteLine();
+            obj3.Display();
+            Console.WriteLine();
+            obj4.Display();
+            Console.WriteLine();
+        }
+    }
+}
