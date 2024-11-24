c727cbf578e7adea67746d0c6e71b44e25f28fcc
shadman76r <shadmansakibalam@gmail.com>
2024-11-25 01:51:11 +0600

    variable

--- compiler_lab/variable_check.cpp
+++ compiler_lab/variable_check.cpp
@@ -32,11 +32,11 @@ int main() {
     return 0;
 }*/
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    string variable="2 2";
+    string variable="name";
    // cout<<"Enter a valid variable name"<<endl;
     //cin>>variable;
 
